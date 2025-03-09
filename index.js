805155f32ac979cbf81c5666d8e988123e8e955a
savannah7704 <savannah7704@comcast.net>
2025-03-09 11:42:17 -0500

    Added function for calculating points

--- index.js
+++ index.js
@@ -13,6 +13,59 @@ app.use(cors({
 
 // We need an app.get function for adding up the points.
 
+const calculatePoints = (age, bmi, bloodPressure, familyDisease) => { // Written by Savannah Stumpf
+  let totalPoints = 0
+
+  if (age < 30) {
+    totalPoints += 0
+  } else if (age < 45) {
+    totalPoints += 10
+  } else if (age < 60) {
+    totalPoints += 20
+  } else {
+    totalPoints += 30
+  }
+
+  if (bmi >= 18.5 && bmi <= 24.9) { // normal
+    totalPoints += 0
+  } else if (bmi >= 25.0 && bmi <= 29.9) { // overweight
+    totalPoints += 30
+  } else { // obesity
+    totalPoints += 75
+  }
+
+  if (bloodPressure == 'normal') {
+    totalPoints += 0
+  } else if (bloodPressure == 'elevated') {
+    totalPoints += 15
+  } else if (bloodPressure == 'stage 1') {
+    totalPoints += 30
+  } else if (bloodPressure == 'stage 2') {
+    totalPoints += 75
+  } else if (bloodPressure == 'crisis') {
+    totalPoints += 100
+  }
+
+  // for family disease, maybe it can be an array? 
+  // Each disease can be searched for in the array
+  if (familyDisease.includes('diabetes')) {
+    totalPoints += 10
+  }
+  if (familyDisease.includes('cancer')) {
+    totalPoints += 10
+  }
+  if (familyDisease.includes('Alzheimer\'s')) {
+    totalPoints += 10
+  }
+
+}
+
+app.get('/calculatePoints', (request, response) => { // Written by Savannah Stumpf
+  console.log('Calling "/totalPoints" on the Node.js server.')
+  const pointTotal = calculatePoints(age, bmi, bloodPressure, familyDisease)
+  response.json({ pointTotal })
+})
+
 // We need an app.get function for calculating BMI.
 
 // Any other calculations also need an app.get function.

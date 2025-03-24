67483943c24168f57561f896323d9329d47ac186
savannah7704 <savannah7704@comcast.net>
2025-03-23 19:29:22 -0500

    Attempt to fix error

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, 'static')));
 
 // CORS config for static site
-const allowedOrigin = 'https://proud-rock-062952f10.6.azurestaticapps.net/';
+const allowedOrigin = 'https://proud-rock-062952f10.6.azurestaticapps.net';
 app.use(cors({
   origin: allowedOrigin
 }));
